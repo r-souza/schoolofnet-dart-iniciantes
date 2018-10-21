9453dbaf0e55105b254010e3674062139e1f5eb1
Rodrigo de Souza <rodrigo.evildead@gmail.com>
2018-10-21 20:18:46 -0300

    Variáveis dinâmicas

---
+++ 03-variaveis-dinamicas.dart
@@ -0,0 +1,21 @@
+void main() {
+  var name;
+
+  name = 'Rodrigo';
+  name = 30;
+  name = 88.2;
+  name = true;
+
+  print(name);
+
+  dynamic nameDynamic;
+
+  nameDynamic = 'Rodrigo';
+  nameDynamic = true;
+  nameDynamic = 30;
+  nameDynamic = 88.2;
+
+  print(nameDynamic);
+  
+
+}
