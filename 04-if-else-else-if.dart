80d3dc0e032e135b8990c51ec17d143a7ec838ce
Rodrigo de Souza <rodrigo.evildead@gmail.com>
2018-10-21 21:12:32 -0300

    Condições

---
+++ 04-if-else-else-if.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  var name = 'Rodrigo';
+
+  if(name == 'Rodrigo'){
+    print ('First if executed');
+  }
+
+  int age = 30;
+
+  if(age >= 18) {
+    print('Adult!');
+  } else if(age >= 13) {
+    print('Teen!');
+  } else {
+    print('Kid!');
+  }
+
+  print('Done');
+}
