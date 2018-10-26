3025d0e49a44e319608d00a426d525226ddce6c7
Rodrigo de Souza <rodrigo.souza@avare.sp.gov.br>
2018-10-26 10:59:26 -0300

    While Do-While

---
+++ 07-while-do-while.dart
@@ -0,0 +1,13 @@
+void main () {
+  int age = 1;
+
+  while (age < 18) {
+    print(age.toString() + ' is not an adult age!');
+    age++;
+  }
+
+
+  do {
+    print('Execute first even verification is false');
+  } while (false);
+}
